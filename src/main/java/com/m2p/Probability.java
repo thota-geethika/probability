5ecdab9eddbd32b6f696a2b60e6270b84d739ced
thota-geethika <geetika.sree@m2pfintech.com>
2023-03-31 15:18:43 +0530

    Requriement 2: Add. Added the required testcases and logic to find the Probability of two events occurring together.

---
+++ src/main/java/com/m2p/Probability.java
@@ -0,0 +1,31 @@
+package com.m2p;
+
+public class Probability {
+
+    private final double probabilityValue;
+
+
+    public Probability(double probabilityValue)
+    {
+        this.probabilityValue = probabilityValue;
+    }
+
+    @Override
+    public boolean equals(Object probabilityObject)
+    {
+        if(this == probabilityObject)
+        {
+            return true;
+        }
+        if(getClass() != probabilityObject.getClass())
+        {
+            return false;
+        }
+        return probabilityValue == ((Probability)probabilityObject).probabilityValue;
+    }
+
+    public Probability and(Probability event)
+    {
+        return new Probability(probabilityValue * event.probabilityValue);
+    }
+}
---
+++ src/test/java/com/m2p/ProbabilityTest.java
@@ -0,0 +1,44 @@
+package com.m2p;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ProbabilityTest {
+
+    @Test
+    void toReturnEqualityWhenProbabilityOfHeadsIsComparedToProbabilityOfTailsInACoinToss()
+    {
+        Probability HeadsProbabilityInACoinToss = new Probability(0.5);
+        Probability TailsProbabilityInACoinToss = new Probability(0.5);
+
+        assertThat(HeadsProbabilityInACoinToss,is(equalTo(TailsProbabilityInACoinToss)));
+    }
+
+    @Test
+    void toReturnProbabilityAsOneWhenOccurringOfTwoEventsIsCertain()
+    {
+        Probability probabilityOfOneEventToHappenCertainly = new Probability(1);
+        Probability probabilityOfAnotherEventToHappenCertainly = new Probability(1);
+
+        Probability twoCertainEventsOccurringTogether = new Probability(1);
+
+        assertThat(twoCertainEventsOccurringTogether,is(equalTo(probabilityOfOneEventToHappenCertainly.and(probabilityOfAnotherEventToHappenCertainly))));
+    }
+
+    // The probability of getting head in one toss is independent of getting toss in another toss
+    @Test
+    void toReturnZeroPointTwoFiveWhenHeadsOccurInOneTossAndHeadsOccurInAnotherToss()
+    {
+        Probability probabilityOfHeadsInOneToss = new Probability(0.5);
+        Probability probabilityOfHeadsInAnotherToss = new Probability(0.5);
+
+        Probability eventsOccurringTogether = new Probability(0.25);
+
+        assertThat(eventsOccurringTogether,is(equalTo(probabilityOfHeadsInOneToss.and(probabilityOfHeadsInAnotherToss))));
+    }
+
+
+}
