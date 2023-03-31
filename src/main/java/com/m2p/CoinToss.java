bddb854fa67b8f7ba68b3b50775141be0094e25b
thota-geethika <geetika.sree@m2pfintech.com>
2023-03-31 12:43:34 +0530

    Requirement 1: Add. Added the required testcases and logic to satisfy the Requirement 1.

--- src/main/java/com/m2p/CoinToss.java
+++ src/main/java/com/m2p/CoinToss.java
@@ -1,4 +1,17 @@
 package com.m2p;
 
 public class CoinToss {
+
+    private final double tossProbability;
+
+    public CoinToss(double tossProbability)
+    {
+        this.tossProbability = tossProbability;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        return tossProbability == ((CoinToss)object).tossProbability;
+    }
 }
--- src/test/java/com/m2p/CoinTossTest.java
+++ src/test/java/com/m2p/CoinTossTest.java
@@ -1,6 +1,19 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class CoinTossTest {
 
-    CoinToss coinObject = new CoinToss();
+    @Test
+    void toReturnEqualityWhenProbabilityOfHeadsIsComparedToProbabilityOfTails()
+    {
+        CoinToss HeadsProbability = new CoinToss(0.5);
+        CoinToss TailsProbability = new CoinToss(0.5);
+
+        assertThat(HeadsProbability,is(equalTo(TailsProbability)));
+    }
 }
